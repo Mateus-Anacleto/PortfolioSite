a81f3f50f26f8a47cc8af2883498fa07ad39ea7a
Mateus Anacleto <mateusanacleto119@gmail.com>
2024-05-28 20:09:49 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,61 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+
+
+
+/*ROLAGEM P/ SERVIÇOS*/
+document.addEventListener('DOMContentLoaded', function() {
+    var servicosLink = document.querySelector('a[href="#servicos"]');
+    if (servicosLink) {
+        servicosLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            var targetElement = document.querySelector('#servicos');
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
+});
+/*ROLAGEM P/ SOBRE*/
+document.addEventListener('DOMContentLoaded', function() {
+    var servicosLink = document.querySelector('a[href="#sobre"]');
+    if (servicosLink) {
+        servicosLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            var targetElement = document.querySelector('#sobre');
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
+});
+/*ROLAGEM P/ E-MAIL*/
+document.addEventListener('DOMContentLoaded', function() {
+    var servicosLink = document.querySelector('a[href="#email"]');
+    if (servicosLink) {
+        servicosLink.addEventListener('click', function(event) {
+            event.preventDefault();
+            var targetElement = document.querySelector('#email');
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    }
+});
+/*REDIRECIONAMENTO PARA WHATSAPP*/
+document.addEventListener('DOMContentLoaded', function() {
+    var whatsappButton = document.getElementById('whatsappButton');
+    if (whatsappButton) {
+        whatsappButton.addEventListener('click', function() {
+            var phoneNumber = '5547992528805';
+            var message = encodeURIComponent('Olá, quero realizar um orçamento. \u{1F604}\u{2699}');
+            var whatsappAppLink = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + message;
+            window.open(whatsappAppLink, '_blank');
+        });
+    }
+});
